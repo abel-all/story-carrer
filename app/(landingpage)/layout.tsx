1b3429b874128771724fdbfec37e9a3b7db2c9cd
abel-all <abel-all@student.1337.ma>
2024-11-03 17:29:56 +0100

    initial commit

---
+++ app/(landingpage)/layout.tsx
@@ -0,0 +1,13 @@
+const LandingPageLayout = ({ children } : { children : React.ReactNode}) => {
+    return (
+    <div>
+        navbar
+        <main>
+        {children}
+        </main>
+        footer
+    </div>
+    )
+};
+
+export default LandingPageLayout;
